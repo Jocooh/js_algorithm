5dc0f2a30fc440039c2d678981df432b0d32cfcc
JOCO <111356420+Jocooh@users.noreply.github.com>
2022-12-16 19:23:32 +0900

    리팩토링

--- findstring.js
+++ findstring.js
@@ -1,12 +1,20 @@
+// function solution(s, t) {
+//   let answer = 0;
+//   for (let x of s) {
+//     if (x === t) {
+//       answer += 1;
+//     }
+//   }
+//   return answer;
+// }
+
+// let str = "COMPUTERPROGRAMMING";
+// console.log(solution(str, "G"));
+
 function solution(s, t) {
-  let answer = 0;
-  for (let x of s) {
-    if (x === t) {
-      answer += 1;
-    }
-  }
-  return answer;
+  let answer = s.split(t).length; //[ 'COMPUTE', 'P', 'OG', 'AMMING' ] =>4
+  return answer - 1;
 }
 
 let str = "COMPUTERPROGRAMMING";
-console.log(solution(str, "G"));
+console.log(solution(str, "R"));
