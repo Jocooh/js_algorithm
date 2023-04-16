1e65d7dc33f9e7de0c6675637ab30532302e1bb8
JOCO <111356420+Jocooh@users.noreply.github.com>
2023-04-16 19:52:15 +0900

    section 2 다른 풀이

--- section2/보이는_학생.js
+++ section2/보이는_학생.js
@@ -11,3 +11,14 @@ function solution(arr) {
 }
 let arr = [130, 135, 148, 140, 145, 150, 150, 153];
 console.log(solution(arr));
+
+//다른 풀이
+// function solution(arr) {
+//   let answer = 1;
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] < arr[i + 1]) answer++;
+//   }
+//   return answer;
+// }
+// let arr = [130, 135, 148, 140, 145, 150, 150, 153];
+// console.log(solution(arr));
