b170789ade4f36030bb6085ee81580eb98df05d3
JOCO <111356420+Jocooh@users.noreply.github.com>
2023-04-20 16:40:19 +0900

    Update 공통원소.js

--- section5/공통원소.js
+++ section5/공통원소.js
@@ -15,3 +15,19 @@ function solution(arr1, arr2) {
 let a = [1, 3, 9, 5, 2]; //1,2,3,5,9
 let b = [3, 2, 5, 7, 8]; //2,3,5,7,8
 console.log(solution(a, b));
+
+//다른풀이
+// function solution(arr1, arr2) {
+//   let answer = [];
+//   let sortarr1 = arr1.sort((a, b) => a - b);
+//   let sortarr2 = arr2.sort((a, b) => a - b);
+
+//   for (let i = 0; i < arr1.length; i++) {
+//     for (let j = 0; j < arr1.length; j++) {
+//       if (sortarr1[i] === sortarr2[j]) {
+//         answer.push(sortarr1[i]);
+//       }
+//     }
+//   }
+//   return answer;
+// }
