7b28f837e64e04cb8533c92b40aa0011ab7756ae
JOCO <111356420+Jocooh@users.noreply.github.com>
2023-05-16 12:28:41 +0900

    section7. 장꾸현수

---
+++ section7/장꾸현수.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+  answer = [];
+  let sortArr = [...arr].sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== sortArr[i]) answer.push(i + 1);
+  }
+  return answer;
+}
+
+let arr = [120, 130, 150, 150, 130, 150];
+console.log(solution(arr));
