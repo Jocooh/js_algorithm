bb9821320015c7c60553092b2c74e066476e9040
JOCO <111356420+Jocooh@users.noreply.github.com>
2023-05-18 15:54:45 +0900

    section4 졸업선물 수정

--- section4/졸업선물.js
+++ section4/졸업선물.js
@@ -1,19 +1,18 @@
 function solution(m, product) {
   let answer = 0;
-  let n = product.length; //5
-  product.sort((a, b) => a[0] + a[1] - b[0] + b[1]);
+  let n = product.length;
+  product.sort((a, b) => a[0] + a[1] - (b[0] + b[1]));
   for (let i = 0; i < n; i++) {
-    let seed = m - (product[i][0] / 2 + product[i][1]); //할인쿠폰을 하나의 경우마다 다 써보자.
+    let money = m - (product[i][0] / 2 + product[i][1]);
     let cnt = 1;
     for (let j = 0; j < n; j++) {
-      if (j !== i && product[j][0] + product[j][1] > seed) break; //헛돌지 않도록 하기 위해서
-      if (j !== i && product[j][0] + product[j][1] <= seed) {
-        //남아있는 돈 보다 가격이 아직 적다면
-        seed -= product[j][0] + product[j][1];
+      if (j !== i && product[j][0] + product[j][1] > money) break;
+      if (j !== i && product[j][0] + product[j][1] <= money) {
+        money -= product[j][0] + product[j][1];
         cnt++;
       }
     }
-    answer = Math.max(answer, cnt); //할인쿠폰을 학생 하나 당 썼을때 나올 수 있는 최대 학생 수 = cnt, 즉 answer 에는 상황에 따라 가장 많은 학생수가 나온다.
+    answer = Math.max(answer, cnt);
   }
   return answer;
 }
